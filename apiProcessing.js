a77e920ca7b4d6f240dd5ab0bd7fd4a38f37b3ea
Matthew Ackard <mattackard@gmail.com>
2018-08-10 21:41:04 -0700

    made race start time a readable time

--- apiProcessing.js
+++ apiProcessing.js
@@ -10,9 +10,20 @@ function validateApiResponse(res) {
   }
 }
 
+
+function convertTime(sec) {
+  let readableTime = new Date(sec * 1000);
+  return readableTime;
+}
+
 function getRaceData(callback) {
   axios.get('http://api.speedrunslive.com/races')
        .then((response) => {
+         response.data.races.forEach((race) => {
+           if (race.time > 0) {
+             race.time = convertTime(race.time);
+           }
+         });
          callback(response.data.races);
        })
        .catch((error) => {
