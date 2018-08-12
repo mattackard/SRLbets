06841433787468444636340008e0a3bc4f196396
Matthew Ackard <mattackard@gmail.com>
2018-08-11 21:21:09 -0700

    updated user schema with more accurate keys

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  twitchUsername: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  points : {
+    type: Number,
+    default: 100,
+    required: true
+  },
+  betHistory : [
+    {
+      raceId : {
+        type: String,
+        required: true
+      },
+      amountBet : {
+        type: Number,
+        required: true
+      },
+      result : {
+        type: String,
+        required: true
+      }
+    }
+  ]
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
