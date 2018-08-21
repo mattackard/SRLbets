dafd6ce48c44dabc37d146e2d6a7ec7dd0feae6b
Matt Ackard <mattackard@gmail.com>
2018-08-21 00:03:51 -0700

    started converting twitch auth to new twitch api

--- public/js/twitch.js
+++ public/js/twitch.js
@@ -1,3 +1,33 @@
+const axios = require('axios');
+const clientId = '36ajloyc79v2ccwny9v8zfog0lwr3z';
+const redirect = 	'http://localhost:3000/twitch';
+
+function getTwitchUserToken (clientId, redirectURL) {
+  axios.get(`https://id.twitch.tv/oauth2/authorize
+    ?client_id=${clientId}
+    &redirect_uri=${redirectURL}
+    &response_type=token
+    &scope=user:read:email`)
+    .then((res) => {
+      //get the auth code from the redirected url via document.location.hash
+      console.log(document.location.hash);
+    })
+    .then(() => {
+      axios.post(`https://id.twitch.tv/oauth2/token
+    ?client_id=${clientId}
+    &client_secret=<your client secret>
+    &code=<authorization code received above>
+    &grant_type=authorization_code
+    &redirect_uri=<your registered redirect URI>`);
+    });
+}
+
+function getTwitchAppToken() {
+  axios.post();
+}
+
+
+
 
 Twitch.init({clientId: '36ajloyc79v2ccwny9v8zfog0lwr3z'}, (error, status) => {
   if (error) {
@@ -10,12 +40,33 @@ Twitch.init({clientId: '36ajloyc79v2ccwny9v8zfog0lwr3z'}, (error, status) => {
     $('.twitch-connect').hide();
     $('.loginStatus').text('You are logged in through Twitch!');
 
-    Twitch.api({method: 'user'}, function(error, user) {
-      $('.twitchUsername').text(user.display_name);
-    });
   }
 
-  $('.twitch-connect').click(function() {         //login through twitch on button click
+  Twitch.api({method: 'user'}, function(error, user) {
+    $('.twitchUsername').text(user.display_name);
+    User.find({ twitchUsername: user.display_name }, (err, res) => {    //add user to db if it doesn't already exist
+      if (err) {
+        throw Error(err);
+      }
+      if (!res.twitchUsername) {
+        let userObj = {
+          twitchUsername: user.display_name,
+          points : 100,
+          betHistory : []
+        };
+        User.create(userObj, (err, user) => {
+          if (err) {
+            throw Error(err);
+          }
+          else {
+            console.log('user was saved to db');
+          }
+        });
+      }
+    });
+  });
+
+  $('.twitch-connect').click(() => {         //login through twitch on button click
     Twitch.login({
       scope: ['user_read', 'channel_read']
     });
