df618a3848186f67676fb5808337b4c361b3effc
Matt Ackard <mattackard@gmail.com>
2018-09-02 18:34:50 -0700

    fixed mobile menu opening/closing

--- public/js/dom.js
+++ public/js/dom.js
@@ -3,9 +3,9 @@ let hamburgerClose = document.getElementById('hamburgerClose');
 let nav = document.querySelector('nav');
 
 hamburgerOpen.addEventListener('click', () => {
-  nav.style.opacity = 1;
+  nav.style.display = 'block';
 });
 
 hamburgerClose.addEventListener('click', () => {
-  nav.style.opacity = 0;
+  nav.style.display = 'none';
 });
