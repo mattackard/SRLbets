a3e93ae73cf9982b61cddc83d59d00b5a12dea68
Matt Ackard <mattackard@gmail.com>
2018-09-02 20:28:08 -0700

    more styling of the mobile navigation and race layout

--- public/js/dom.js
+++ public/js/dom.js
@@ -9,3 +9,24 @@ hamburgerOpen.addEventListener('click', () => {
 hamburgerClose.addEventListener('click', () => {
   nav.style.display = 'none';
 });
+
+
+//race layout stuff
+let entrantDropDown = document.getElementsByClassName('entrantDropDown');
+
+Array.from(entrantDropDown).forEach((element) => {
+  element.addEventListener('click', (e) => {
+    let entrant = e.target.nextSibling;
+    let buttonText = e.target.innerText;
+    buttonText = buttonText.substring(0, buttonText.length - 2);
+    console.log(buttonText);
+    if (entrant.style.display === 'block') {
+      entrant.style.display = 'none';
+      e.target.innerText = `${buttonText} ${String.fromCharCode(9660)}`;
+    }
+    else {
+      entrant.style.display = 'block';
+      e.target.innerText = `${buttonText} ${String.fromCharCode(9650)}`;
+    }
+  });
+});
