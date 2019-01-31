f8bfd84bf82b8e8aed79bad520b95b5b8db8d57e
Matt Ackard <mattackard@gmail.com>
2019-01-30 21:48:23 -0800

    setup create-react-app and express boilerplate

--- test/sanity_test.js
+++
@@ -1,9 +0,0 @@
-const expect = require("chai").expect;
-
-describe("SANITY CHECKS", () => {
-	describe("Chai integration", () => {
-		it("chai can run a test from a file in the test folder", () => {
-			expect(true).to.be.ok;
-		});
-	});
-});
