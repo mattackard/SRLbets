10739e3b2db8ddf715aadad41459daed7ea1985c
Matt Ackard <mattackard@gmail.com>
2019-02-14 19:26:59 -0800

    added promises to twitch login

--- backend/twitch.js
+++ backend/twitch.js
@@ -23,90 +23,127 @@ const hash = x =>
 		.update(x, "utf8")
 		.digest("hex");
 
-function getUserFromTwitch(req, code, state, callback) {
+const getUserFromTwitch = (req, code, state, callback) => {
 	if (state !== hash(req.session.id)) {
-		console.error("request state does not match response state");
+		throw Error(
+			"Returned OAuth state does not match state sent in initial request"
+		);
 	} else {
-		//getUserAccess().then(getUserData()).then(getUserFollows()).then(getUsersFromId(followList)).then(saveUser()).catch()
-
 		//gets oAuth tokens and user data from Twitch
 		axios
 			.post(
 				`https://id.twitch.tv/oauth2/token?client_id=${twitchClientId}&client_secret=${twitchClientSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${twitchRedirect}`,
 				{ withCredentials: true }
 			)
-			.then(token => {
-				if (token.status !== 200) {
-					console.error("error in access token request");
-					handleTwitchError(token);
-				} else {
-					//gets the user information using the token information retrieved in the parent request
-					axios
-						.get("https://api.twitch.tv/helix/users", {
-							headers: {
-								Authorization: `Bearer ${
-									token.data.access_token
-								}`,
-							},
-							withCredentials: true,
-						})
-						//saves the user data into db session and user db
-						.then(userData => {
-							if (userData.status !== 200) {
-								console.error("error in user request");
-								handleTwitchError(userData);
-							}
-							userData.following = [];
-							//updates session storage with twitch API data
-							req.session.username = userData.data.data[0].login;
-							req.session.access_token = token.data.access_token;
-							req.session.refresh_token =
-								token.data.refresh_token;
-							req.session.token_type = token.data.token_type;
-							req.session.twitchUserId = userData.data.data[0].id;
-							req.session.save();
-							getUserFollowsFromAPI(
-								userData.data.data[0].id,
-								"",
-								[],
-								followList => {
-									return saveUser(
-										userData,
-										token,
-										followList,
-										callback
-									);
-								}
-							);
-						});
-				}
+			.then(data => getUserData(data, req))
+			.then(getUserFollowsFromAPI)
+			// .then(getUsersFromId)
+			.then(saveUser)
+			.then(callback)
+			.catch(err => {
+				throw Error(err);
 			});
 	}
-}
+};
 
-function saveUser(userData, tokens, followList, callback) {
-	//if the user doesnt already exists one is created
-	User.findOne(
-		{ twitchUsername: userData.data.data[0].login },
-		(err, doc) => {
+const getUserData = (tokenObj, req) => {
+	//gets the user information using the token information retrieved initial auth request
+	return new Promise((resolve, reject) => {
+		axios
+			.get("https://api.twitch.tv/helix/users", {
+				headers: {
+					Authorization: `Bearer ${tokenObj.data.access_token}`,
+				},
+				withCredentials: true,
+			})
+			.then(userData => {
+				//saves user info into session store
+				req.session.username = userData.data.data[0].login;
+				req.session.access_token = tokenObj.data.access_token;
+				req.session.refresh_token = tokenObj.data.refresh_token;
+				req.session.token_type = tokenObj.data.token_type;
+				req.session.twitchUserId = userData.data.data[0].id;
+				req.session.save();
+				resolve(userData.data.data[0]);
+			})
+			.catch(err => {
+				reject(err);
+			});
+	});
+};
+
+//gets the user ids for all twitch users followed by the signed in user
+const getUserFollowsFromAPI = (
+	userData,
+	lastPagination = "",
+	followList = []
+) => {
+	return new Promise((resolve, reject) => {
+		//sets pagination string, get url, and updated follow list for recursive calls
+		let follows = followList;
+		let twitchId = userData.id;
+		let pagination = lastPagination;
+
+		//adds the pagination string and adds it to the request URL if it exists
+		let getUrl = pagination
+			? `https://api.twitch.tv/helix/users/follows?from_id=${twitchId}&after=${pagination}`
+			: `https://api.twitch.tv/helix/users/follows?from_id=${twitchId}`;
+
+		axios
+			.get(getUrl, {
+				headers: { "Client-ID": `${twitchClientId}` },
+				withCredentials: true,
+			})
+			.then(response => {
+				//sets the new pagination string for the next request
+				pagination = response.data.pagination.cursor;
+				response.data.data.forEach(follow => {
+					//get user data from ID?
+					follows.push(follow.to_id);
+				});
+				//continues to get more following ids until the total has been reached
+				if (follows.length < response.data.total) {
+					resolve(
+						getUserFollowsFromAPI(userData, pagination, follows)
+					);
+				}
+				//runs the callback to send the complete unpaginated data
+				else if (follows.length === response.data.total) {
+					userData.followList = follows;
+					resolve(userData);
+				} else {
+					reject(
+						"Something went wrong with getting user follows ... the generated list is longer than twitch's recorded follows for user"
+					);
+				}
+			})
+			.catch(err => {
+				reject(err);
+			});
+	});
+};
+
+const saveUser = userData => {
+	return new Promise((resolve, reject) => {
+		//if the user doesnt already exists one is created
+		User.findOne({ twitchUsername: userData.login }, (err, doc) => {
 			if (err) {
 				err.message = "Could not find user in DB for saveUser function";
-				throw Error(err);
+				reject(err);
 			}
 			if (!doc) {
 				User.create(
 					{
-						twitchUsername: userData.data.data[0].login,
-						avatar: userData.data.data[0].profile_image_url,
-						oAuth: tokens.data,
-						following: followList,
+						twitchUsername: userData.login,
+						avatar: userData.profile_image_url,
+						following: userData.followList,
 					},
 					(err, saved) => {
 						if (err) {
-							throw Error(err);
+							reject(err);
 						} else {
 							console.log("user was saved");
-							callback(saved);
+							resolve(saved);
 						}
 					}
 				);
@@ -114,67 +151,27 @@ function saveUser(userData, tokens, followList, callback) {
 				console.log("user already exists, updated data is being sent");
 				User.updateOne(
 					{
-						avatar: userData.data.data[0].profile_image_url,
-						oAuth: tokens.data,
-						following: followList,
+						avatar: userData.profile_image_url,
+						following: userData.followList,
 					},
-					err => {
+					(err, saved) => {
 						if (err) {
-							throw Error(err);
+							reject(err);
 						} else {
-							callback(doc);
+							resolve(saved);
 						}
 					}
 				);
 			}
-		}
-	);
-}
-
-//gets the user ids for all twitch users followed by the signed in user
-function getUserFollowsFromAPI(
-	twitchId,
-	lastPagination = "",
-	followList = [],
-	callback
-) {
-	//sets pagination string, get url, and updated follow list for recursive calls
-	let follows = followList;
-	let pagination = lastPagination;
-	let getUrl = pagination
-		? `https://api.twitch.tv/helix/users/follows?from_id=${twitchId}&after=${pagination}`
-		: `https://api.twitch.tv/helix/users/follows?from_id=${twitchId}`;
-	axios
-		.get(getUrl, {
-			headers: { "Client-ID": `${twitchClientId}` },
-			withCredentials: true,
-		})
-		.then(response => {
-			//sets the new pagination string for the next request
-			pagination = response.data.pagination.cursor;
-			response.data.data.forEach(follow => {
-				//get user data from ID?
-				follows.push(follow.to_id);
-			});
-			//continues to get more following ids until the total has been reached
-			if (follows.length < response.data.total) {
-				getUserFollowsFromAPI(twitchId, pagination, follows, callback);
-			}
-			//runs the callback to send the complete unpaginated data
-			if (follows.length === response.data.total) {
-				callback(follows);
-			}
-		})
-		.catch(err => {
-			throw Error(err);
 		});
-}
+	});
+};
 
-function handleTwitchError(res) {
+const handleTwitchError = (req, res) => {
 	switch (res.status) {
 		//unauthorized
 		case 401:
-			handleUnauthorized();
+			handleUnauthorized(req);
 		//ok
 		case 200:
 			return res;
@@ -186,17 +183,15 @@ function handleTwitchError(res) {
 			throw Error(res);
 	}
 	throw Error(res);
-}
+};
 
-function handleUnauthorized() {
+const handleUnauthorized = req => {
 	console.log("called unauthorized");
 	User.find({ twitchUsername: req.session.username }, (err, user) => {
-		let res = tokenRefresh(
-			user.oAuth.access_token,
-			user.oAuth.refresh_token,
-			twitchClientId,
-			twitchClientSecret
-		);
+		if (err) {
+			throw Error(err);
+		}
+		let res = tokenRefresh(req, user.oAuth.refresh_token);
 		if (res.status === 200) {
 			user.oAuth.access_token = res.access_token;
 			user.oAuth.refresh_token = res.refresh_token;
@@ -210,23 +205,27 @@ function handleUnauthorized() {
 			//redirect to twitch auth page
 		}
 	});
-}
+};
 
-//needs state added for request
-function tokenRefresh(refresh) {
+//requests a new twitch access token using the session's refresh token
+const tokenRefresh = (req, refresh) => {
 	console.log("attempting to refresh token");
+	const state = hash(req.session.id);
 	axios
 		.post(
 			`https://id.twitch.tv/oauth2/token--data-urlencode?grant_type=refresh_token&refresh_token=${encodeURIComponent(
 				refresh
-			)}&client_id=${twitchClientId}&client_secret=${twitchClientSecret}`,
+			)}&client_id=${twitchClientId}&client_secret=${twitchClientSecret}&state=${state}`,
 			{ withCredentials: true }
 		)
 		.then((err, res) => {
 			if (err) throw Error("Error in token refresh request");
 			return res;
+		})
+		.catch(err => {
+			throw Error(err);
 		});
-}
+};
 
 module.exports.getUserFromTwitch = getUserFromTwitch;
 module.exports.hash = hash;
