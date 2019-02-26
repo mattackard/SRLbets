016a7cf4d0ca6e32968d0229b8e0148b65edff47
Matt Ackard <mattackard@gmail.com>
2019-02-25 21:00:06 -0800

    multitwitch links and show all entrants button added

--- client/src/Components/RaceTile/index.js
+++ client/src/Components/RaceTile/index.js
@@ -1,45 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.scss";
 
 import RaceTileEntrant from "../RaceTileEntrant";
 
-const RaceTile = props => {
-	let multiTwitch = "http://multitwitch.tv";
-	let race = props.race;
+class RaceTile extends Component {
+	state = {
+		showAllEntrants: false,
+	};
 
-	let entrantKeys = Object.keys(race.entrants);
-	return (
-		<div className="race">
-			<h1>{race.gameTitle}</h1>
-			<p className="race-goal">{race.goal || "No Goal Set"}</p>
-			<p>{race.status}</p>
-			{race.status !== "Entry Open" && race.status !== "Entry Closed" ? (
-				<p>Started {race.simpleTime}</p>
-			) : null}
-			<p>
-				{/* determines whether entrant needs to be plural */}
-				{entrantKeys.length === 1
-					? `${entrantKeys.length} Race Entrant`
-					: `${entrantKeys.length} Race Entrants`}
-			</p>
-			<div className="race-entrants">
-				{entrantKeys.map(entrant => (
-					<RaceTileEntrant
-						key={race.entrants[entrant].name}
-						entrant={race.entrants[entrant]}
-						raceStatus={race.status}
-						raceID={race.raceID}
-						getDataFromDb={props.getDataFromDb}
-					/>
-				))}
+	toggleAllEntrants = () => {
+		this.setState({
+			showAllEntrants: !this.state.showAllEntrants,
+		});
+	};
+
+	render() {
+		let race = this.props.race;
+		let multiTwitch = "http://multitwitch.tv";
+		let entrantLimit = 5;
+
+		let entrantKeys = Object.keys(race.entrants);
+		return (
+			<div className="race">
+				<h1>{race.gameTitle}</h1>
+				<p className="race-goal">{race.goal || "No Goal Set"}</p>
+				<p>{race.status}</p>
+				{race.status !== "Entry Open" &&
+				race.status !== "Entry Closed" ? (
+					<p>Started {race.simpleTime}</p>
+				) : null}
+				<p>
+					{/* determines whether entrant needs to be plural */}
+					{entrantKeys.length === 1
+						? `${entrantKeys.length} Race Entrant`
+						: `${entrantKeys.length} Race Entrants`}
+				</p>
+				<div className="race-entrants">
+					{entrantKeys.map(entrant => {
+						multiTwitch += race.entrants[entrant].twitch
+							? `/${race.entrants[entrant].twitch}`
+							: "";
+						if (entrantLimit > 0 || this.state.showAllEntrants) {
+							entrantLimit--;
+							return (
+								<RaceTileEntrant
+									key={race.entrants[entrant].name}
+									entrant={race.entrants[entrant]}
+									raceStatus={race.status}
+									raceID={race.raceID}
+									getDataFromDb={this.props.getDataFromDb}
+								/>
+							);
+						} else {
+							return null;
+						}
+					})}
+					{entrantKeys.length > 5 ? (
+						<button onClick={this.toggleAllEntrants}>
+							{this.state.showAllEntrants ? "...less" : "...more"}
+						</button>
+					) : null}
+				</div>
+				{multiTwitch !== "http://multitwitch.tv" &&
+				entrantKeys.length > 1 ? (
+					<a
+						href={multiTwitch}
+						target="_blank"
+						rel="noopener noreferrer">
+						MultiTwtich Link
+					</a>
+				) : null}
 			</div>
-			{multiTwitch === "http://multitwitch.tv" ? null : (
-				<a href={multiTwitch} target="_blank" rel="noopener noreferrer">
-					MultiTwtich Link
-				</a>
-			)}
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default RaceTile;
