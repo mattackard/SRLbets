129c904d39c722ede57518450e4b0741ec4a540a
Matthew Ackard <mattackard@gmail.com>
2019-04-05 15:54:51 -0700

    removed raceHostiroy schema, fixed typos

--- models/game.js
+++ models/game.js
@@ -1,44 +1,30 @@
 const mongoose = require("mongoose");
 
-const RaceHistory = new mongoose.Schema({
-	//needs implemented
-	raceID: { type: String, required: true },
-	goal: String,
-	status: { type: String, required: true },
-	time: Number,
-	timeStarted: { type: Date, required: true },
-	simpleTime: String,
-	betTotal: { type: Number, required: true, default: 0 },
-});
-
 const GameCategory = new mongoose.Schema({
 	//needs implemented
 	goal: { type: String, required: true },
 	avgTime: { type: Number, required: true },
-	winRatio: { type: Number, required: true },
 	mostWins: {
 		srlName: String,
-		twitcUsername: String,
+		twitchUsername: String,
 		numWins: Number,
 	},
 	mostEntries: {
 		srlName: String,
-		twitcUsername: String,
+		twitchUsername: String,
 		numEntries: Number,
 	},
 	bestTime: {
 		srlName: String,
-		twitcUsername: String,
+		twitchUsername: String,
 		time: Number,
 	},
 });
 
 const GameSchema = new mongoose.Schema({
-	//needs implemented
 	gameID: { type: Number, required: true },
 	gameTitle: { type: String, required: true, trim: true },
-	raceHistory: [RaceHistory],
-	betTotal: { type: Number, required: true, default: 0 },
+	raceHistory: { type: Array, of: Number, default: new Array() },
 	categories: { type: Map, of: GameCategory, default: new Map() }, //goal as map key
 });
 
